19321b4e351857c182d67d11286b06c21480890f
hoehoe <hoehoetester1@gmail.com>
2020-05-24 22:48:33 +1200

    Authenticate Route

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
+const Joi = require('@hapi/joi');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const schema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(2).required(),
+});
 
 /**
  * @route  GET api/auth
@@ -14,8 +23,49 @@ router.get('/', (req, res) => {
  * @desc   Auth user & get token
  * @access Public
  */
-router.post('/', (req, res) => {
-  res.send('Log in user');
+router.post('/', async (req, res) => {
+  const { error } = await schema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ msg: 'Email does not exist' });
+    }
+
+    const isMatchPassword = await bcrypt.compare(password, user.password);
+    if (!isMatchPassword) {
+      return res.status(400).json({ msg: 'Invalid Password' });
+    }
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.jwtSecret,
+      {
+        expiresIn: '1h',
+      },
+      (err, token) => {
+        if (err) {
+          throw err;
+        }
+
+        res.status(200).json({ token });
+      }
+    );
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error...');
+  }
 });
 
 module.exports = router;
