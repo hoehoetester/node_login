724ae7ed3513f0295796f7ba40a9a298735d1b70
hoehoe <hoehoetester1@gmail.com>
2020-05-25 23:37:37 +1200

    Register UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('nnn');
+  const [email, setEmail] = useState('eee');
+  const [password, setPassword] = useState('ppp');
+
+  const onNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onPasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='name'>Name: </label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              value={name}
+              onChange={onNameChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='email'>Email: </label>
+            <input
+              type='text'
+              id='email'
+              name='email'
+              value={email}
+              onChange={onEmailChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='password'>Password: </label>
+            <input
+              type='text'
+              id='password'
+              name='password'
+              value={password}
+              onChange={onPasswordChange}
+            />
+          </div>
+          <div>
+            <input type='submit' value='Register' />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
